8b58ffe86dea6790f185968769715cf66b2160c5
Adel <redpenaa@yandex.ru>
2022-12-02 10:55:24 +0300

    добавил поиск через бек, пагинацию

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Card from "../components/Card";
 import PlaceHolder from "../components/PlaceHolder";
+import Pagination from "../components/PaginationComponent/Pagination";
 
 const Home = ({ searchValue }) => {
   //https://6383693c6e6c83b7a992dead.mockapi.io/items
@@ -15,13 +16,14 @@ const Home = ({ searchValue }) => {
   });
   const [orderType, setOrderType] = useState("asc");
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const search = searchValue ? `&search=${searchValue}` : "";
+  const category = categoryId > 0 ? `category=${categoryId}` : "";
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
-      `https://6383693c6e6c83b7a992dead.mockapi.io/items?${
-        categoryId > 0 ? `category=${categoryId}` : ""
-      }&sortBy=${sortType.sortProperty}&order=${orderType}`
+      `https://6383693c6e6c83b7a992dead.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortType.sortProperty}&order=${orderType}${search}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -29,27 +31,21 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, orderType]);
-  const cards = items
-    .filter((obj) => {
-      if (obj.title.toLowerCase().includes(searchValue.toLowerCase()))
-        return true;
-      return false;
-    })
-    .map((obj) =>
-      isLoading ? (
-        <PlaceHolder />
-      ) : (
-        <Card
-          key={obj.id}
-          title={obj.title}
-          price={obj.price}
-          imageUrl={obj.imageUrl}
-          sizes={obj.sizes}
-          types={obj.types}
-        />
-      )
-    );
+  }, [categoryId, sortType, orderType, searchValue, currentPage]);
+  const cards = items.map((obj) =>
+    isLoading ? (
+      <PlaceHolder />
+    ) : (
+      <Card
+        key={obj.id}
+        title={obj.title}
+        price={obj.price}
+        imageUrl={obj.imageUrl}
+        sizes={obj.sizes}
+        types={obj.types}
+      />
+    )
+  );
   const placeHolders = [...new Array(6)].map((_, index) => (
     <PlaceHolder key={index} />
   ));
@@ -69,6 +65,8 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? placeHolders : cards}</div>
+
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
